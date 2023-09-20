0023a7483306187b457db1bd8906d1dd798d4b01
Tyler Jackson <tyler.j.jackson@outlook.com>
2023-09-20 09:57:43 -0700

    Prod url missing www.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment'
 
 export const title = 'Tyler Jackson Dev'
 export const description = 'Tyler Jackson, Software Engineer Website'
-export const url = dev ? 'http://localhost:5173/' : 'https://tylerjjackson.com/'
+export const url = dev ? 'http://localhost:5173/' : 'https://www.tylerjjackson.com/'
